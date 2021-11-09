fcfef14c50e6cf4d1fb44b52f2e457bb9bf4f097
dpk <contactdpk1@gmail.com>
2021-11-09 22:35:49 +0530

    top-nav complete

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-console.log("hello world");
+let searchInput=document.querySelector(".nav__search_text");searchInput.addEventListener("focusin",(()=>{document.querySelector(".nav__search").classList.add("focus")})),searchInput.addEventListener("focusout",(()=>{document.querySelector(".nav__search").classList.remove("focus")}));let options=document.querySelector("#categories"),label=document.querySelector("#selected_categories");options.addEventListener("change",(()=>{label.innerHTML=options.selectedOptions[0].text}));
 //# sourceMappingURL=script.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+let searchInput = document.querySelector(".nav__search_text");
+
+searchInput.addEventListener("focusin",()=>{
+    document.querySelector(".nav__search").classList.add("focus");
+
+})
+searchInput.addEventListener("focusout",()=>{
+    document.querySelector(".nav__search").classList.remove("focus");
+
+})
+
+
+let options = document.querySelector("#categories");
+let label = document.querySelector("#selected_categories")
+
+options.addEventListener("change",()=>{
+label.innerHTML=options.selectedOptions[0].text;
+})
