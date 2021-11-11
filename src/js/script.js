e24b0cbee3dd79135e3c35c86570fe751a8fedc7
dpk <contactdpk1@gmail.com>
2021-11-11 18:59:29 +0530

    code refinement

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 let searchInput = document.querySelector(".nav__search_text");
 
 searchInput.addEventListener("focusin",()=>{
-    document.querySelector(".nav__search").classList.add("focus");
+    document.querySelector(".nav__search").classList.add("nav-focus");
 
 })
 searchInput.addEventListener("focusout",()=>{
-    document.querySelector(".nav__search").classList.remove("focus");
+    document.querySelector(".nav__search").classList.remove("nav-focus");
 
 })
 
