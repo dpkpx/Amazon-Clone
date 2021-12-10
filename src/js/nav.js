9b2c8c57fd00fc8bf4349585e8c8a47fecaa11d7
dpkpx <contactdpk1@gmail.com>
2021-12-10 20:10:44 +0530

    background dull when prime in focus

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,8 @@
 const searchInput = document.querySelector(".nav__search_text");
 const options = document.querySelector("#categories");
-const label = document.querySelector("#selected_categories")
+const label = document.querySelector("#selected_categories");
+const primeLink = document.querySelector(".nav2__menu_list_link--prime");
+const covers = document.querySelectorAll(".cover");
 
 
 searchInput.addEventListener("focusin", () => {
@@ -15,6 +17,14 @@ searchInput.addEventListener("focusout", () => {
 
 options.addEventListener("change", () => {
     label.innerHTML = options.selectedOptions[0].text;
+})
+
+primeLink.addEventListener("mouseover",()=>{
+ covers.forEach(cover=>cover.classList.add("focusOut"));
+})
+
+primeLink.addEventListener("mouseout",()=>{
+    covers.forEach(cover=>cover.classList.remove("focusOut"));
 })
 
 
